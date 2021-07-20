ffd9865d9c589c3fab9606d09fbb4912115425c4
Heyson <hs@heysonseverino.com>
2021-07-20 14:08:04 -0400

    Update Component Header with props

--- react-essentials/src/App.js
+++ react-essentials/src/App.js
@@ -1,10 +1,37 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+function Header(props) {
+  console.log(props);
+  return(
+    <header>
+      <h1>{props.name} Kitchen</h1>
+  </header>
+  );
+}
+
+function Main() {
+  return (
+    <section>
+      <p>We serve the most delicious food around.</p>
+    </section>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+      <p>It's true</p>
+    </footer>    
+  )
+}
 
 function App() {
   return (
     <div className="App">
-    
+      <Header name="Joe"/>
+      <Main />
+      <Footer />
     </div>
   );
 }
