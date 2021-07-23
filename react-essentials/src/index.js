086e73785d862bc307e2a8134943572b8417cd71
Heyson <hs@heysonseverino.com>
2021-07-23 10:32:52 -0400

    Understanding the useState Hook

--- react-essentials/src/index.js
+++ react-essentials/src/index.js
@@ -3,11 +3,5 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from "./App";
 
-const [,, light] = ["boots", "tent", "headlamp"];
-console.log(light);
-
-ReactDOM.render(
-<App authorized={false} />, 
-document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
