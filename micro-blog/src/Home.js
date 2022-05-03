f4068ba19ae8345aa57b7f0d74976cfca48dd4d9
Heyson <hs@heysonseverino.com>
2022-05-03 10:58:30 -0400

    Click Events

--- micro-blog/src/Home.js
+++ micro-blog/src/Home.js
@@ -1,7 +1,15 @@
 const Home = () => {
+    const handleClick = (e) => {
+        console.log('hello World', e);
+    }
+    const handleClickAgain = (name, e) => {
+        console.log('hello ' + name);
+    }
     return (
         <div className ="home">
             <h2>Home Page</h2>
+            <button onClick={handleClick}>Click me</button>
+            <button onClick={(e) => handleClickAgain('Joe', e)}>Click me again</button>
         </div>
     );
 }
