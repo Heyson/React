56f8ecb0bbe01831d419a30dc207ac043b0ed4fb
Heyson <hs@heysonseverino.com>
2022-05-05 10:27:37 -0400

    filter blogs for author attribute to show author 'heyson'

--- micro-blog/src/Home.js
+++ micro-blog/src/Home.js
@@ -4,14 +4,16 @@ import BlogList from "./BlogList";
 const Home = () => { 
     // blogs array using React state and hook
     const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+        { title: 'My new website', body: 'lorem ipsum...', author: 'heyson', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
     return (
-    // cycles through blogs array and maps each object and outputs title: and author: with JSX template
+    // cycles through blogs array and maps each object and outputs title: and author: with JSX template.
+    // filter blogs for author attribute to show author 'heyson'.
         <div className ="home">
            <BlogList blogs={blogs} title="All Blogs!"/>
+           <BlogList blogs={blogs.filter((blog) => blog.author==='heyson')} title="Heyson's blogs"/>
         </div>
     );
 }
