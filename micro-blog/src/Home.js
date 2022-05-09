7bd9898ae9aabc18affb29a8c40dc49b134c74ec
Heyson <hs@heysonseverino.com>
2022-05-09 10:04:04 -0400

    Add Conditional Loading Message

--- micro-blog/src/Home.js
+++ micro-blog/src/Home.js
@@ -4,28 +4,30 @@ import BlogList from "./BlogList";
 const Home = () => { 
     // blogs array using React state and hook
     const [blogs, setBlogs] = useState(null);
-
-    const [name, setName] = useState('mario');
+    const [isPending, setIsPending] = useState(true);
 
     // fetches data from JSON object to populate state in "blogs"
     useEffect(() => {
-        fetch('http://localhost:8000/blogs')
+        //setTimeout performs function after 1000ms to simulate long request time to show Loading... message
+        setTimeout(() => {
+            fetch('http://localhost:8000/blogs')
             //response object, returns a promise
             .then(res => {
                 return res.json();
             })
             // gets the data
             .then(data => {
-                console.log(data);
                 setBlogs(data);
-            })
+                setIsPending(false);
+            });
+        }, 1000);
     },     []);
 
     return (
     // cycles through blogs array and maps each object and outputs title: and author: with JSX template.
         <div className ="home">
-        
-           {blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
+            {isPending && <div>Loading...</div>}
+            {blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
         </div>
     );}
 
