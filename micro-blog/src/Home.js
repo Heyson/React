6a1251dc60bcc0ce6bea6c2a080afb050b640883
Heyson <hs@heysonseverino.com>
2022-05-09 10:20:56 -0400

    Handling Fetch Errors

--- micro-blog/src/Home.js
+++ micro-blog/src/Home.js
@@ -5,6 +5,7 @@ const Home = () => {
     // blogs array using React state and hook
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null)
 
     // fetches data from JSON object to populate state in "blogs"
     useEffect(() => {
@@ -12,20 +13,32 @@ const Home = () => {
         setTimeout(() => {
             fetch('http://localhost:8000/blogs')
             //response object, returns a promise
-            .then(res => {
-                return res.json();
-            })
-            // gets the data
-            .then(data => {
-                setBlogs(data);
-                setIsPending(false);
-            });
+                .then(res => {
+                    console.log(res);
+                     if(!res.ok){
+                        throw Error('Could not fetch data for that resource')
+                     }
+                    return res.json();
+                })
+                // gets the data
+                .then(data => {
+                    setBlogs(data);
+                    setIsPending(false);
+                    setError(null);
+                })
+                //catches network error and fires a function
+                .catch(err => {
+                    setIsPending(false);
+                    setError(err.message);
+                })
+
         }, 1000);
     },     []);
 
     return (
     // cycles through blogs array and maps each object and outputs title: and author: with JSX template.
         <div className ="home">
+            {error && <div>{ error }</div>}
             {isPending && <div>Loading...</div>}
             {blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
         </div>
