ae389c21ef4bc419b00dfe586c64533edefa2695
Heyson <hs@heysonseverino.com>
2022-05-09 09:23:59 -0400

    implement useEffect Dependencies, establish JSON Server

--- micro-blog/src/Home.js
+++ micro-blog/src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => { 
@@ -9,11 +9,18 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
     ]);
 
+    const [name, setName] = useState('mario');
+
     // prop that deletes blog
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);
         setBlogs(newBlogs);
     }
+
+    useEffect(() => {
+        console.log('use effect ran');
+        console.log(name);
+    }, [name]);
 
     return (
     // cycles through blogs array and maps each object and outputs title: and author: with JSX template.
@@ -21,6 +28,8 @@ const Home = () => {
         <div className ="home">
            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
            <BlogList blogs={blogs.filter((blog) => blog.author==='heyson')} title="Heyson's blogs" handleDelete={handleDelete}/>
+           <button onClick={() => setName('luigi')}>change name</button>
+           <p>{ name }</p>
         </div>
     );
 }
