62827f962c12b93b06f0e752d092d1b30e14f38d
Heyson <hs@heysonseverino.com>
2022-05-10 13:14:54 -0400

    :

--- micro-blog/src/Home.js
+++ micro-blog/src/Home.js
@@ -1,11 +1,10 @@
-import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 import useFetch from "./useFetch";
+import React from "react";
 
 const Home = () => { 
-    const { data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs')
+    const { data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
    
-
     return (
     // cycles through blogs array and maps each object and outputs title: and author: with JSX template.
         <div className ="home">
