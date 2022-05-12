d3304be9d6fa45599415e23755503761df494b23
Heyson <hs@heysonseverino.com>
2022-05-12 16:09:07 -0400

    Add Blog add and delete components

--- micro-blog/src/Home.js
+++ micro-blog/src/Home.js
@@ -1,17 +1,17 @@
+import React from "react";
 import BlogList from "./BlogList";
 import useFetch from "./useFetch";
-import React from "react";
 
-const Home = () => { 
-    const { data: blogs, isPending, error} = useFetch('http://localhost:8000/blogs');
-   
-    return (
-    // cycles through blogs array and maps each object and outputs title: and author: with JSX template.
-        <div className ="home">
-            {error && <div>{ error }</div>}
-            {isPending && <div>Loading...</div>}
-            {blogs && <BlogList blogs={blogs} title="All Blogs!"/>}
-        </div>
-    );}
+const Home = () => {
+  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
 
+  return (
+    <div className="home">
+      { error && <div>{ error }</div> }
+      { isPending && <div>Loading...</div> }
+      { blogs && <BlogList blogs={blogs} /> }
+    </div>
+  );
+}
+ 
 export default Home;
--- micro-blog/src/Navbar.js
+++ micro-blog/src/Navbar.js
@@ -1,15 +1,21 @@
 import React from "react";
 
-const Navbar = () => {
-    return (
-        <nav className="navbar">
-            <h1>The Blog</h1>
-            <div className="links">
-                <a href="/">Home</a>
-                <a href="/create">New Blog</a>
-            </div>
-        </nav>
-    );
-}
+import { Link } from "react-router-dom";
 
-export default Navbar ;
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <h1>The Dojo Blog</h1>
+      <div className="links">
+        <Link to="/">Home</Link>
+        <Link to="/create" style={{ 
+          color: 'white', 
+          backgroundColor: '#f1356d',
+          borderRadius: '8px' 
+        }}>New Blog</Link>
+      </div>
+    </nav>
+  );
+}
+ 
+export default Navbar;
